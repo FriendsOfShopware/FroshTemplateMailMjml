79a9fa6ae3adcd2ccb22a54ce2c487ed1fbb68a0
Shyim <s.sayakci@shopware.com>
2018-06-09 19:36:27 +0200

    Remove local mjml installation check, fixes #1

--- FroshTemplateMailMjml.php
+++ FroshTemplateMailMjml.php
@@ -13,15 +13,4 @@ use Shopware\Components\Plugin\Context\InstallContext;
  */
 class FroshTemplateMailMjml extends Plugin
 {
-    /**
-     * @param InstallContext $context
-     *
-     * @author Soner Sayakci <shyim@posteo.de>
-     */
-    public function install(InstallContext $context)
-    {
-        if (MjmlCompiler::isMjmlInstalled() === null) {
-            throw new \RuntimeException('MJML must be installed and accessable using $PATH');
-        }
-    }
 }
